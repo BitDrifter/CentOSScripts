f6cc755b17cf4a94bb04200d5720e9f5effc7427
BitDrifter <bitdrifter@gmail.com>
2015-07-04 18:36:44 -0700

    Add command to update global bashrc file to source git-completion.bash

--- git_scripts/add_git_cli_completion.bash
+++ git_scripts/add_git_cli_completion.bash
@@ -3,17 +3,37 @@ printError() {
   printf '\E[31m'; echo "$@"; printf '\E[0m'
 }
 
+#Function to print yellow error text in terminal
+printWarning() {
+  printf '\E[33m'; echo "$@"; printf '\E[0m'
+}
+
 #Script needs to be run as root, so verify this.
 if [[ $EUID -ne 0 ]]; then
-  printError "This script should be run using sudo or as the root user"
+  printError "Error: This script needs to be run using sudo or as the root user"
   exit 1
 fi
 
 #Navigate to directory where bash loads its completion scripts
 cd /etc/bash_completion.d 
 
-#Download the git completion script
+#Download the lastet git completion script
 curl -OL https://github.com/git/git/raw/master/contrib/completion/git-completion.bash
 
 #Load the completion script now so we don't have to wait for a restart
 source git-completion.bash
+
+#Change to directory where the global bashrc is located
+cd /etc
+
+BASHRC_FILE="/etc/bashrc"
+SOURCE_GIT_COMPLETION_COMMAND="source /etc/bash_completion.d/git-completion.bash"
+
+#Check if bashrc has already been updated to load git-completion.bash
+if grep -Fxq $SOURCE_GIT_COMPLETION_COMMAND $BASHRC_FILE
+then
+  #Modify the global bashrc to load the git-completion script
+  echo $SOURCE_GIT_COMPLETION_COMMAND >> $BASHRC_FILE
+else
+  echo "Warning: Global bashrc already updated to load git-completion.bash"
+fi
